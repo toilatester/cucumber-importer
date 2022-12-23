7916f39552db0cd954ca1bf40e697ba4cd7996fe
minhhoangvn <hnminh@outlook.com>
2022-12-23 16:12:17 +0700

    feat: init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const {IMPORTER_TYPE} = require('./src/importer');
-// const {TEST_INFO_MAPPER_TYPE} = require('./src/importer/testinfo');
+const {IMPORTER_TYPE} = require('./src/importer');
+const {TEST_INFO_MAPPER_TYPE} = require('./src/importer/testinfo');
 const log4js = require('log4js');
 const logger = log4js.getLogger('main');
 logger.level = 'info';
@@ -14,14 +14,76 @@ program
   .version('0.1.0');
 
 program
-  .command('import')
+  .command('import-xray-jira-cloud')
   .description('Import list of cucumber feature files to test management tool')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
+  .requiredOption('-jh --jira-host <host>', 'The domain value for Jira')
+  .requiredOption(
+    '-ju --jira-username <username>',
+    'The username to authenticate Jira',
+  )
+  .requiredOption(
+    '-jp --jira-password <password>',
+    'The password to authenticate Jira',
+  )
+  .requiredOption(
+    '-xp --xray-project-id <projectId>',
+    'The project ID to import Cucumber test ' +
+      '[https://docs.getxray.app/display/XRAYCLOUD/Importing+Cucumber+Tests+-+REST]',
+  )
+  .requiredOption(
+    '-xh --xray-host <host>',
+    'The domain value for Xray',
+    'xray.cloud.getxray.app',
+  )
+  .requiredOption(
+    '-xu --xray-username <username|clientId>',
+    'The username/clientId to authenticate Xray ' +
+      '[https://docs.getxray.app/display/XRAYCLOUD/Global+Settings%3A+API+Keys]',
+  )
+  .requiredOption(
+    '-xt --xray-token <token|password>',
+    'The token|password to authenticate Xray [https://docs.getxray.app/display/XRAYCLOUD/Global+Settings%3A+API+Keys]',
+  )
+  .requiredOption(
+    '-i, --import-list-file-path <filePath>',
+    'Path to the file contains list of cucumber file for importing to tets management',
+  )
+  .option(
+    '-c, --test-management-config-file-path <filePath>',
+    'Path to the configuration file for import cucumber with extra fields to tets management',
+  )
+  .action((options) => {
+    logger.info(
+      'Start to import cucumber feature files to Xray in Jira Cloud',
+      options,
+    );
+    const {
+      importListFilePath,
+      testManagementConfigFilePath,
+      jiraHost,
+      jiraUsername,
+      jiraPassword,
+      xrayHost,
+      xrayUsername,
+      xrayToken,
+      xrayProjectId,
+    } = options;
+    process.env.XRAY_CLIENT_ID = xrayUsername;
+    process.env.XRAY_CLIENT_SECRET = xrayToken;
+    process.env.XRAY_PROJECT_ID = xrayProjectId;
+    process.env.JIRA_HOST = jiraHost;
+    process.env.JIRA_USERNAME = jiraUsername;
+    process.env.JIRA_TOKEN = jiraPassword;
+    new IMPORTER_TYPE.XRAY_CLOUD(
+      importListFilePath,
+      {
+        host: xrayHost,
+        clientId: xrayUsername,
+        clientSecret: xrayToken,
+        projectId: xrayProjectId,
+      },
+      testManagementConfigFilePath,
+    ).importCucumberToTestManagement(TEST_INFO_MAPPER_TYPE.JIRA);
   });
 
 program
@@ -29,9 +91,8 @@ program
   .description(
     'Query an extract information for preparing test management config file',
   )
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
+  .requiredOption('--first', 'display just the first substring')
+  .requiredOption('-s, --separator <char>', 'separator character', ',')
   .action((str, options) => {
     const limit = options.first ? 1 : undefined;
     console.log(str.split(options.separator, limit));
