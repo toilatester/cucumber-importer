031c5384381b138872e6c31c2c64005c717fed71
minhhoangvn <hnminh@outlook.com>
2022-12-25 20:52:21 +0700

    feat: init

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const {IMPORTER_TYPE} = require('./src/importer');
 const {TEST_INFO_MAPPER_TYPE} = require('./src/importer/testinfo');
-const {TEST_MANAGEMENT_TYPE} = require('./src/test-management');
 const {validateEnvironmentVariables} = require('./src/utils/validation-utils');
+const {
+  getQueryJiraRequiredData,
+  getXrayRequiredData,
+  initJiraClient,
+  validateQueryJiraRequiredData,
+  validateXRayRequiredData,
+} = require('./src/utils/command-utils');
 const inquirer = require('inquirer');
 const log4js = require('log4js');
 const logger = log4js.getLogger('main');
@@ -25,11 +31,6 @@ program
   .option('-ju --jira-username <username>', 'The username to authenticate Jira')
   .option('-jp --jira-password <password>', 'The password to authenticate Jira')
   .option(
-    '-xp --xray-project-id <projectId>',
-    'The project ID to import Cucumber test ' +
-      '[https://docs.getxray.app/display/XRAYCLOUD/Importing+Cucumber+Tests+-+REST]',
-  )
-  .option(
     '-xh --xray-host <host>',
     'The domain value for Xray',
     'xray.cloud.getxray.app',
@@ -44,35 +45,31 @@ program
     'The token|password to authenticate Xray [https://docs.getxray.app/display/XRAYCLOUD/Global+Settings%3A+API+Keys]',
   )
   .option(
-    '-i, --import-list-file-path <filePath>',
-    'Path to the file contains list of cucumber file for importing to tets management',
+    '-xp --xray-project-id <projectId>',
+    'The project ID to import Cucumber test ' +
+      '[https://docs.getxray.app/display/XRAYCLOUD/Importing+Cucumber+Tests+-+REST]',
   )
   .option(
     '-c, --test-management-config-file-path <filePath>',
     'Path to the configuration file for import cucumber with extra fields to tets management',
   )
+  .requiredOption(
+    '-i, --import-list-file-path <filePath>',
+    'Path to the file contains list of cucumber file for importing to tets management',
+  )
   .action((options) => {
-    logger.info(
-      'Start to import cucumber feature files to Xray in Jira Cloud',
-      options,
-    );
+    logger.info('Start to import cucumber feature files to Xray in Jira Cloud');
+    validateXRayRequiredData(options);
+    validateQueryJiraRequiredData(options);
+    getQueryJiraRequiredData(options);
     const {
       importListFilePath,
       testManagementConfigFilePath,
-      jiraHost,
-      jiraUsername,
-      jiraPassword,
       xrayHost,
       xrayUsername,
       xrayToken,
       xrayProjectId,
-    } = options;
-    process.env.XRAY_CLIENT_ID = xrayUsername;
-    process.env.XRAY_CLIENT_SECRET = xrayToken;
-    process.env.XRAY_PROJECT_ID = xrayProjectId;
-    process.env.JIRA_HOST = jiraHost;
-    process.env.JIRA_USERNAME = jiraUsername;
-    process.env.JIRA_TOKEN = jiraPassword;
+    } = getXrayRequiredData(options);
     new IMPORTER_TYPE.XRAY_CLOUD(
       importListFilePath,
       {
@@ -94,7 +91,7 @@ program
   .option('-ju --jira-username <username>', 'The username to authenticate Jira')
   .option('-jp --jira-password <password>', 'The password to authenticate Jira')
   .action(async (options) => {
-    logger.info('Get all projects in Jira Cloud with option', options);
+    logger.info('Start to get all projects in Jira Cloud with option');
     const jira = initJiraClient(options);
     const projects = await jira.getAllJiraProjects();
     for (const project of projects) {
@@ -115,7 +112,7 @@ program
   .option('-ju --jira-username <username>', 'The username to authenticate Jira')
   .option('-jp --jira-password <password>', 'The password to authenticate Jira')
   .action(async (options) => {
-    logger.info('Get all fields in Jira Cloud with option', options);
+    logger.info('Start to get all fields in Jira Cloud');
     const jira = initJiraClient(options);
     const fields = await jira.getAllJiraFields();
     for (const field of fields) {
@@ -140,7 +137,7 @@ program
   .option('-ju --jira-username <username>', 'The username to authenticate Jira')
   .option('-jp --jira-password <password>', 'The password to authenticate Jira')
   .action(async (options) => {
-    logger.info('Get all fields in Jira Cloud with option', options);
+    logger.info('Start to get all fields in Jira Cloud with option');
     const jira = initJiraClient(options);
     await jira.getAllJiraFieldOptions();
   });
@@ -168,40 +165,6 @@ program
 
 program.parse();
 
-function initJiraClient(options) {
-  validateQueryJiraRequiredData(options);
-  const {jiraHost, jiraUsername, jiraPassword} =
-    getQueryJiraRequiredData(options);
-  return new TEST_MANAGEMENT_TYPE.JIRA_CLOUD({
-    host: jiraHost,
-    username: jiraUsername,
-    token: jiraPassword,
-  });
-}
-
 async function askUserInput(prompt, question) {
   return await prompt([question]);
-}
-
-function validateQueryJiraRequiredData(options) {
-  let {jiraHost, jiraUsername, jiraPassword} = options;
-  jiraHost = jiraHost || process.env.JIRA_HOST;
-  jiraUsername = jiraUsername || process.env.JIRA_USERNAME;
-  jiraPassword = jiraPassword || process.env.JIRA_TOKEN;
-  const missingRequiredData = !jiraHost || !jiraUsername || !jiraPassword;
-  if (missingRequiredData) {
-    throw new Error(
-      'Please input value for Jira host with [-jh], Jira username with [-ju] and jiraToken with [-jp].' +
-        'Or use case use .env file for setting the environemnt variables with values ' +
-        '[JIRA_HOST, JIRA_USERNAME, JIRA_TOKEN] ',
-    );
-  }
-}
-
-function getQueryJiraRequiredData(options) {
-  let {jiraHost, jiraUsername, jiraPassword} = options;
-  jiraHost = jiraHost || process.env.JIRA_HOST;
-  jiraUsername = jiraUsername || process.env.JIRA_USERNAME;
-  jiraPassword = jiraPassword || process.env.JIRA_TOKEN;
-  return {jiraHost, jiraUsername, jiraPassword};
 }
---
+++ src/utils/command-utils.js
@@ -0,0 +1,95 @@
+const {TEST_MANAGEMENT_TYPE} = require('../test-management');
+const log4js = require('log4js');
+const logger = log4js.getLogger('main');
+logger.level = 'info';
+require('dotenv').config();
+
+function initJiraClient(options) {
+  validateQueryJiraRequiredData(options);
+  const {jiraHost, jiraUsername, jiraPassword} =
+    getQueryJiraRequiredData(options);
+  return new TEST_MANAGEMENT_TYPE.JIRA_CLOUD({
+    host: jiraHost,
+    username: jiraUsername,
+    token: jiraPassword,
+  });
+}
+
+function validateQueryJiraRequiredData(options) {
+  let {jiraHost, jiraUsername, jiraPassword} = options;
+  jiraHost = jiraHost || process.env.JIRA_HOST;
+  jiraUsername = jiraUsername || process.env.JIRA_USERNAME;
+  jiraPassword = jiraPassword || process.env.JIRA_TOKEN;
+  const missingRequiredData = !jiraHost || !jiraUsername || !jiraPassword;
+  if (missingRequiredData) {
+    throw new Error(
+      'Please input value for Jira host with [-jh], Jira username with [-ju] and jiraToken with [-jp]. \n' +
+        'Or using .env file for setting the environemnt variables with values ' +
+        '[JIRA_HOST, JIRA_USERNAME, JIRA_TOKEN].',
+    );
+  }
+}
+
+function getQueryJiraRequiredData(options) {
+  let {jiraHost, jiraUsername, jiraPassword} = options;
+  jiraHost = jiraHost || process.env.JIRA_HOST;
+  jiraUsername = jiraUsername || process.env.JIRA_USERNAME;
+  jiraPassword = jiraPassword || process.env.JIRA_TOKEN;
+  process.env.JIRA_HOST = jiraHost;
+  process.env.JIRA_USERNAME = jiraUsername;
+  process.env.JIRA_TOKEN = jiraPassword;
+  return {jiraHost, jiraUsername, jiraPassword};
+}
+
+function validateXRayRequiredData(options) {
+  let {xrayHost, xrayUsername, xrayToken, xrayProjectId} = options;
+  xrayHost = xrayHost || process.env.XRAY_HOST;
+  xrayUsername = xrayUsername || process.env.XRAY_CLIENT_ID;
+  xrayToken = xrayToken || process.env.XRAY_CLIENT_SECRET;
+  xrayProjectId = xrayProjectId || process.env.XRAY_PROJECT_ID;
+  const missingRequiredData =
+    !xrayHost || !xrayUsername || !xrayToken || !xrayProjectId;
+  if (missingRequiredData) {
+    throw new Error(
+      'Please input value for Xray host with [-xh], Xray username|clientId with [-xu], Xray token with [-xt] and' +
+        'Xray project id with [-xp]. \n' +
+        'Or using .env file for setting the environemnt variables with values ' +
+        '[XRAY_HOST, XRAY_CLIENT_ID, XRAY_CLIENT_SECRET, XRAY_PROJECT_ID].',
+    );
+  }
+}
+
+function getXrayRequiredData(options) {
+  let {
+    importListFilePath,
+    testManagementConfigFilePath,
+    xrayHost,
+    xrayUsername,
+    xrayToken,
+    xrayProjectId,
+  } = options;
+  xrayHost = xrayHost || process.env.XRAY_HOST;
+  xrayUsername = xrayUsername || process.env.XRAY_CLIENT_ID;
+  xrayToken = xrayToken || process.env.XRAY_CLIENT_SECRET;
+  xrayProjectId = xrayProjectId || process.env.XRAY_PROJECT_ID;
+  process.env.XRAY_CLIENT_ID = xrayUsername;
+  process.env.XRAY_CLIENT_SECRET = xrayToken;
+  process.env.XRAY_PROJECT_ID = xrayProjectId;
+  process.env.XRAY_HOST = xrayHost;
+  return {
+    xrayHost,
+    xrayUsername,
+    xrayToken,
+    xrayProjectId,
+    importListFilePath,
+    testManagementConfigFilePath,
+  };
+}
+
+module.exports = {
+  initJiraClient,
+  validateQueryJiraRequiredData,
+  getQueryJiraRequiredData,
+  validateXRayRequiredData,
+  getXrayRequiredData,
+};
